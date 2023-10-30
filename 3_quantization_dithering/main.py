2e43b23f7387f636f3af63de0c720d1a5d2be6ea
Miłosz Zubala <zm49455@zut.edu.pl>
2023-10-30 21:09:45 +0100

    Prepared stubs for dithering

--- 3_quantization_dithering/main.py
+++ 3_quantization_dithering/main.py
@@ -97,38 +97,47 @@ def n_bit_greyscale_pallet(n: int=2):
     bits = 2**n
     return np.linspace(0,1,bits).reshape(bits,1)
 
-images = [
-    #color
-    'SMALL_0001.tif', 
-    'SMALL_0004.jpg',
-    'SMALL_0005.jpg',
-    'SMALL_0006.jpg',
-    'SMALL_0007.jpg',
-    'SMALL_0008.jpg',
-    'SMALL_0009.jpg',
-    'SMALL_0010.jpg',
-    # greyscale
-    'GS_0001.tif',
-    'GS_0002.png',
-    'GS_0003.png',
-    'SMALL_0002.png',
-    'SMALL_0003.png',
-]
+def dithering_random(img:np.ndarray, Pallet:np.ndarray) -> np.ndarray:
+    return img
+
+def dithering_ordered(img:np.ndarray, Pallet:np.ndarray) -> np.ndarray:
+    return img
+
+def dithering_floyd_steinberg(img:np.ndarray, Pallet:np.ndarray) -> np.ndarray:
+    return img
 
 
 if __name__ == "__main__":
     
-    columns_quantization = ["filename", "greyscale"]
-    data_quantization = [
-                        ['GS_0001.tif', True],
-                        ['GS_0002.png', True],
-                        # ['GS_0003.png', True],
-                        # ['SMALL_0009.jpg', False],
-                        # ['SMALL_0007.jpg', False],
-                        # ['SMALL_0006.jpg', False],
-                        # ['SMALL_0005.jpg', False]
+    columns = ["filename", "greyscale", "pallets"]
+    data = [
+                        ['GS_0001.tif', True, {"pallet_1_bit":n_bit_greyscale_pallet(1),
+                                               "pallet_2_bit":n_bit_greyscale_pallet(2),
+                                               "pallet_4_bit":n_bit_greyscale_pallet(4)}],
+
+                        # ['GS_0002.png', True, {"pallet_1_bit":n_bit_greyscale_pallet(1),
+                        #                        "pallet_2_bit":n_bit_greyscale_pallet(2),
+                        #                        "pallet_4_bit":n_bit_greyscale_pallet(4)}],
+
+                        # ['GS_0003.png', True, {"pallet_1_bit":n_bit_greyscale_pallet(1),
+                        #                        "pallet_2_bit":n_bit_greyscale_pallet(2),
+                        #                        "pallet_4_bit":n_bit_greyscale_pallet(4)}],
+
+                        # ['SMALL_0009.jpg', False, {"pallet_8_bit":pallet8,
+                        #                            "pallet_16_bit":pallet16}],
+
+                        # ['SMALL_0007.jpg', False, {"pallet_8_bit":pallet8,
+                        #                            "pallet_16_bit":pallet16}],
+
+                        # ['SMALL_0006.jpg', False, {"pallet_8_bit":pallet8,
+                        #                            "pallet_16_bit":pallet16}],
+
+                        # ['SMALL_0005.jpg', False, {"pallet_8_bit":pallet8,
+                        #                            "pallet_16_bit":pallet16}],
                         ]
-    df_quantization = pd.DataFrame(data=data_quantization, columns=columns_quantization)
+    df = pd.DataFrame(data=data, columns=columns)
+
+
 
     document = Document()
     for section in document.sections:
@@ -138,34 +147,72 @@ if __name__ == "__main__":
         section.right_margin = Cm(0.5)
     document.add_heading('Lab3 - Milosz Zubala (zm49455)', 0)
 
-    document.add_heading(f"Kwantyzacja",1)
-    for index, row in df_quantization.iterrows():
+    document.add_heading(f"Kwantyzacja i dithering",1)
+    for index, row in df.iterrows():
         current_image = plt.imread(os.path.join(INPUT_DIR, row['filename']))
         current_image = imgToFloat(current_image)
 
+        document.add_heading(f"Zdjecie: {row['filename']}",2)
         if row['greyscale']==True:
             if len(current_image.shape)>2:
                 current_image = calcY1(current_image)
-            p1 = kwant_colorFit(current_image, n_bit_greyscale_pallet(1))
-            p2 = kwant_colorFit(current_image, n_bit_greyscale_pallet(2))
-            p4 = kwant_colorFit(current_image, n_bit_greyscale_pallet(4))
-            fig,ax = show_photos([current_image, p1, p2, p4],
-                        ['original', 'Pallet 1-bit','Pallet 2-bit','Pallet 4-bit'],
-                        True)
-        else:
-            p8 = kwant_colorFit(current_image, pallet8)
-            p16 = kwant_colorFit(current_image, pallet16)
-            fig,ax = show_photos([current_image, p8, p16],
-                        ['original','Pallet 8-bit','Pallet 16-bit'],
-                        False)
-        fig.suptitle(f"Quantization")
+            
+            for p_name, pallet in row['pallets'].items():
+                
+                document.add_heading(f"Paleta {p_name}",3)
 
-        memfile = BytesIO()
-        fig.savefig(memfile)
-        document.add_heading(f"Zdjecie: {row['filename']}",2)
-        document.add_picture(memfile)
-        memfile.close()
-        
+                data = [current_image]
+                labels = ['original']
+
+                data.append(kwant_colorFit(current_image, pallet))
+                labels.append('quantization')
+
+                if len(np.unique(pallet)) == 2:
+                    data.append(dithering_floyd_steinberg(current_image, pallet))
+                    labels.append('dith random')
+                
+                data.append(dithering_ordered(current_image, pallet))
+                labels.append('dith ordered')
+
+                data.append(dithering_floyd_steinberg(current_image, pallet))
+                labels.append('dith floyd stteinberg')
+
+                fig,ax = show_photos(data, labels, True)
+
+                fig.suptitle(p_name)
+                memfile = BytesIO()
+                fig.savefig(memfile)
+                
+                document.add_picture(memfile)
+                memfile.close()
+            
+        else:
+            
+            for p_name, pallet in row['pallets'].items():
+                
+                document.add_heading(f"Paleta {p_name}",3)
+
+                data = [current_image]
+                labels = ['original']
+
+                data.append(kwant_colorFit(current_image, pallet))
+                labels.append('quantization')
+
+                data.append(dithering_ordered(current_image, pallet))
+                labels.append('dith ordered')
+
+                data.append(dithering_floyd_steinberg(current_image, pallet))
+                labels.append('dith floyd stteinberg')
+
+                fig,ax = show_photos(data, labels, True)
+
+                fig.suptitle(p_name)
+                memfile = BytesIO()
+                fig.savefig(memfile)
+                
+                document.add_picture(memfile)
+                memfile.close()
+
 
     document.save(os.path.normpath(os.path.join(__file__,'..','output','lab3_milosz_zubala.docx')))
-    # plt.show()
+    plt.show()
