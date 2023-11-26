2d8d4e548f7e4cd436f62be59f18d81a1e7677f2
Milosz Zubala <miloszzubala@gmail.com>
2023-11-26 13:33:59 +0100

    RLE, byterun finished

--- 5_RLE_ByteRun/main.py
+++ 5_RLE_ByteRun/main.py
@@ -11,15 +11,15 @@ from docx.shared import Inches, Cm
 
 INPUT_DIR = os.path.normpath(os.path.join(__file__,'..','input'))
 
-TEST1 = np.array([1,1,1,1,2,1,1,1,1,2,1,1,1,1])
-TEST2 = np.array([1,2,3,1,2,3,1,2,3])
-TEST3 = np.array([5,1,5,1,5,5,1,1,5,5,1,1,5])
-TEST4 = np.array([-1,-1,-1,-5,-5,-3,-4,-2,1,2,2,1])
-TEST5 = np.zeros((1,520))
-TEST6 = np.arange(0,521,1)
-TEST7 = np.eye(7)
-TEST8 = np.dstack([np.eye(7),np.eye(7),np.eye(7)])
-TEST9 = np.ones((1,1,1,1,1,1,10))
+TEST1 = np.array([1,1,1,1,2,1,1,1,1,2,1,1,1,1]).astype(int)
+TEST2 = np.array([1,2,3,1,2,3,1,2,3]).astype(int)
+TEST3 = np.array([5,1,5,1,5,5,1,1,5,5,1,1,5]).astype(int)
+TEST4 = np.array([-1,-1,-1,-5,-5,-3,-4,-2,1,2,2,1]).astype(int)
+TEST5 = np.zeros((1,520)).astype(int)
+TEST6 = np.arange(0,521,1).astype(int)
+TEST7 = np.eye(7).astype(int)
+TEST8 = np.dstack([np.eye(7),np.eye(7),np.eye(7)]).astype(int)
+TEST9 = np.ones((1,1,1,1,1,1,10)).astype(int)
 
 TEST_SUITE = [
     TEST1,
@@ -97,7 +97,7 @@ def encode_RLE(data: np.ndarray):
     x=np.concatenate([x, data.shape])
 
     data_copy = data_copy.flatten()
-    bufor = np.zeros(int(np.prod(data.shape)*2,))
+    bufor = np.zeros(int(np.prod(data.shape)*2,)).astype(int)
 
     start = 0
     bufor_current_index = 0
@@ -108,10 +108,9 @@ def encode_RLE(data: np.ndarray):
         repetitions, start = index_same_numbers(start, data_copy)
         bufor[[bufor_current_index, bufor_current_index+1]] = [repetitions, current_element]
         bufor_current_index += 2
-        # print(repetitions, current_element)
         
 
-    bufor = np.concatenate([x, bufor])
+    bufor = np.concatenate([x, bufor[:bufor_current_index]])
     return bufor
 
 def decode_RLE(data: np.ndarray):
@@ -163,9 +162,7 @@ def encode_ByteRun(data: np.ndarray):
             repetitions, start = repetitions_tmp, start_tmp
             bufor_current_index += repetitions + 1
 
-
-    print(bufor)
-    bufor = np.concatenate([x, bufor])
+    bufor = np.concatenate([x, bufor[:bufor_current_index]])
     return bufor
 
 def decode_ByteRun(data: np.ndarray):
@@ -199,9 +196,13 @@ def decode_ByteRun(data: np.ndarray):
 
     return bufor
 
- 
-if __name__ == "__main__":
+def imgToUInt8(img):
+    
+    if np.issubdtype(img.dtype, np.floating):
+        img = (img*255).astype(int)
+    return img   
 
+if __name__ == "__main__":
     document = Document()
     for section in document.sections:
         section.top_margin = Cm(1)
@@ -210,48 +211,198 @@ if __name__ == "__main__":
         section.right_margin = Cm(0.5)
     document.add_heading('Lab5 - Milosz Zubala (zm49455)', 0)
 
-    document.add_heading('Testy', 1)
+    document.add_heading('Data payload', 1)
+    document.add_paragraph(f"Dane są kompresowane do 1-wymiarowego obiektu np.ndarray. Na początku tablicy jest załączona informacja o ilości wymiarów, a za nią ich rozmiary.")
+
+    document.add_heading('Tests', 1)
 
     document.add_heading('RLE', 2)
-    for test_vector in TEST_SUITE:
+    for index, test_vector in enumerate(TEST_SUITE):
 
-        document.add_heading(f'{test_vector}', 3)
+        document.add_heading(f'Test {index+1}', 3)
 
         encoded = encode_RLE(test_vector)
         decoded = decode_RLE(encoded)
         equal = np.all(test_vector == decoded)
+        assert(equal)
 
         before_compr = get_size(test_vector)
         after_compr = get_size(encoded)
         cr = np.abs(before_compr/after_compr)
         pr = np.abs(after_compr/before_compr)*100
 
-        print(f"Test Vector\n",test_vector)
-        print("Encoded Vector\n", encoded)
-        print("Decoded Vector\n",decoded)
-        print("Differences", equal)
-        assert(equal)
-        print(f"CR: {cr}, PR: {pr}")
+        document.add_heading(f"Test Vector", 4)
+        document.add_paragraph(str(test_vector))
 
-    for test_vector in TEST_SUITE:
+        document.add_heading(f"Encoded Vector", 4)
+        document.add_paragraph(str(encoded))
+
+        document.add_heading(f"Decoded Vector", 4)
+        document.add_paragraph(f"{decoded}")
+
+        document.add_heading("Metrics", 4)
+        document.add_paragraph(f"Are equal = {equal}")
+        document.add_paragraph(f"Before compression = {before_compr}")
+        document.add_paragraph(f"After compression = {after_compr}")
+        document.add_paragraph(f"CR = {cr}")
+        document.add_paragraph(f"PR = {pr}%")
+
+
+    document.add_heading('ByteRun', 2)
+    for index, test_vector in enumerate(TEST_SUITE):
+
+        document.add_heading(f'Test {index+1}', 3)
 
         encoded = encode_ByteRun(test_vector)
         decoded = decode_ByteRun(encoded)
         equal = np.all(test_vector == decoded)
+        assert(equal)
 
         before_compr = get_size(test_vector)
         after_compr = get_size(encoded)
         cr = np.abs(before_compr/after_compr)
         pr = np.abs(after_compr/before_compr)*100
 
-        print(f"Test Vector\n",test_vector)
-        print("Encoded Vector\n", encoded)
-        print("Decoded Vector\n",decoded)
-        print("Differences", equal)
-        assert(equal)
-        print(f"CR: {cr}, PR: {pr}")
+        document.add_heading(f"Test Vector", 4)
+        document.add_paragraph(str(test_vector))
 
-PHOTOS = ["dokument.png", "kolorowe.png", "rysunek.png"]
+        document.add_heading(f"Encoded Vector", 4)
+        document.add_paragraph(str(encoded))
+
+        document.add_heading(f"Decoded Vector", 4)
+        document.add_paragraph(f"{decoded}")
+
+        document.add_heading("Metrics", 4)
+        document.add_paragraph(f"Are equal = {equal}")
+        document.add_paragraph(f"Before compression = {before_compr}")
+        document.add_paragraph(f"After compression = {after_compr}")
+        document.add_paragraph(f"CR = {cr}")
+        document.add_paragraph(f"PR = {pr}%")
+
+    PHOTOS = ["dokument.png", "kolorowe.png", "techniczny.png"]
+
+    document.add_heading(f'Photos', 1)
+
+    document.add_heading(f'RLE', 2)
+    for index, photo_name in enumerate(PHOTOS):
+
+        document.add_heading(f'Photo {index+1}', 3)
+        current_image = imgToUInt8(plt.imread(os.path.join(INPUT_DIR, photo_name)))
+        
+        encoded = encode_RLE(current_image)
+        decoded = decode_RLE(encoded)
+        equal = np.all(current_image == decoded)
+        assert(equal)
+
+        before_compr = get_size(current_image)
+        after_compr = get_size(encoded)
+        cr = np.abs(before_compr/after_compr)
+        pr = np.abs(after_compr/before_compr)*100
+        uniq = len(np.unique(current_image.flatten()))
+        shape = current_image.shape
+
+        fig, ax = plt.subplots(1,2)
+
+        ax[0].imshow(current_image)
+        ax[1].imshow(decoded)
+
+        fig.suptitle(photo_name)
+        memfile = BytesIO()
+        fig.savefig(memfile)
+        document.add_picture(memfile)
+        memfile.close()
+
+        # document.add_heading(f"Data", 4)
+        # document.add_paragraph(str(current_image))
+
+        document.add_heading(f"Encoded Vector", 4)
+        document.add_paragraph(str(encoded))
+
+        # document.add_heading(f"Decoded Vector", 4)
+        # document.add_paragraph(f"{decoded}")
+
+        document.add_heading("Metrics", 4)
+        document.add_paragraph(f"Are equal = {equal}")
+        document.add_paragraph(f"Before compression = {before_compr}")
+        document.add_paragraph(f"After compression = {after_compr}")
+        document.add_paragraph(f"CR = {cr}")
+        document.add_paragraph(f"PR = {pr}%")
+        document.add_paragraph(f"Shape = {shape}")
+        document.add_paragraph(f"Unique values = {uniq}")
+
+        fig, ax = plt.subplots(1,1)
+
+        d = current_image.flatten()
+        ax.hist(d, bins = uniq)
+
+        fig.suptitle("Values")
+        memfile = BytesIO()
+        fig.savefig(memfile)
+        document.add_picture(memfile)
+        memfile.close()
+
+    document.add_heading(f'ByteRun', 2)
+    for index, photo_name in enumerate(PHOTOS):
+
+        document.add_heading(f'Photo {index+1}', 3)
+        current_image = imgToUInt8(plt.imread(os.path.join(INPUT_DIR, photo_name)))
+        
+        encoded = encode_ByteRun(current_image)
+        decoded = decode_ByteRun(encoded)
+        equal = np.all(current_image == decoded)
+        assert(equal)
+
+        before_compr = get_size(current_image)
+        after_compr = get_size(encoded)
+        cr = np.abs(before_compr/after_compr)
+        pr = np.abs(after_compr/before_compr)*100
+        uniq = len(np.unique(current_image.flatten()))
+        shape = current_image.shape
+
+        fig, ax = plt.subplots(1,2)
+
+        ax[0].imshow(current_image)
+        ax[1].imshow(decoded)
+
+        fig.suptitle(photo_name)
+        memfile = BytesIO()
+        fig.savefig(memfile)
+        document.add_picture(memfile)
+        memfile.close()
+
+        # document.add_heading(f"Data", 4)
+        # document.add_paragraph(str(current_image))
+
+        document.add_heading(f"Encoded Vector", 4)
+        document.add_paragraph(str(encoded))
+
+        # document.add_heading(f"Decoded Vector", 4)
+        # document.add_paragraph(f"{decoded}")
+
+        document.add_heading("Metrics", 4)
+        document.add_paragraph(f"Are equal = {equal}")
+        document.add_paragraph(f"Before compression = {before_compr}")
+        document.add_paragraph(f"After compression = {after_compr}")
+        document.add_paragraph(f"CR = {cr}")
+        document.add_paragraph(f"PR = {pr}%")
+        document.add_paragraph(f"Shape = {shape}")
+        document.add_paragraph(f"Unique values = {uniq}")
+
+        fig, ax = plt.subplots(1,1)
+
+        d = current_image.flatten()
+        ax.hist(d, bins = uniq)
+
+        fig.suptitle("Values")
+        memfile = BytesIO()
+        fig.savefig(memfile)
+        document.add_picture(memfile)
+        memfile.close()
+
+document.add_heading(f'Obserwacje', 1)
+document.add_paragraph(f"Kompresja RLE dla ciągu danych, które się różnią na sąsiadujących pozycjach może zabierać nawet 2 razy więcej pamięci. ByteRun naprawia ten problem poprzez możliwość przepisania n różnych bitów. Kompresje najlepiej radzą sobie dla jednolitego tła o tym samym kolorze - wzzór dokumentu.Próba kodowania ciągle zmieniających się bajtów ostatecznie zabiera więcej miejsca.")
 
 document.save(os.path.normpath(os.path.join(__file__,'..','output','lab5_milosz_zubala.docx')))
+
+
 
